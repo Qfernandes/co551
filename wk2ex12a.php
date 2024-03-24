a74a0debd5a0857a79f9c412304f2cb741425c83
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-03-24 17:27:17 +0000

    amended

--- wk2ex12a.php
+++ wk2ex12a.php
@@ -2,20 +2,24 @@
 $mymarks["year 1"] = 55;
 $mymarks["year 2"] = 65;
 $mymarks["year 3"] = 75;
-  
 ?>
+
+<html>
+
 <head>
 <title>Data </title>
 </head>
+
 <body>
 <table border=1 align="center">
 <tr><th>Year</th><th>Marks</th></tr> 
+
 <?php
   foreach($mymarks as $year => $marks) {
     echo "<tr><td>$year</td><td>$marks</td></tr>";
 }
-
 ?>
 </table>
+
 </body>
 </html>
