f85d2fe61d74bca8f31d86e38d509e91106ce1b2
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-03-27 10:07:32 +0000

    amended

--- savemonster.php
+++ savemonster.php
@@ -1,15 +1,19 @@
 <?php
   
+  // Connected to the database
+
   $db = mysqli_connect("localhost", "root", "", "databaselogbook5");
 
-// Obtain the file sent to the server within the response.
+
+// The monster image and audio files
 $image = $_FILES['monsterimage']['tmp_name']; 
 $audio = $_FILES['monsteraudio']['tmp_name'];
 
-  // Get the file binary data
+  // The image and audio binary data
   $imagedata = addslashes(fread(fopen($image, "r"), filesize($image)));
   $audiodata = addslashes(fread(fopen($audio, "r"), filesize($audio)));
    
+ //Insert in the monster table
   $sql = "INSERT INTO monster";
   $sql .= "(name, image, audio) ";
   $sql .= "VALUES ('$_POST[txtname]', '$imagedata','$audiodata');";
